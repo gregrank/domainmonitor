e5a3c58deae21bdb4a72ebbb0263273a9c66cf14
Greg Rank <greg@gregrank.us>
2015-02-02 22:14:59 -0800

    V3.3

---
+++ ping.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+source /home/cosmo/.bashrc
+curl -I http://gregrank.com
+curl -I http://twenty2onegolf.com
+curl -I http://twenty2one.com
---
+++ ping3.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+source /home/cosmo/.bashrc
+/home/cosmo/ping.sh>home
+mail < home grank@bigsong.com -s "Ping"
